2d94435b2242a496bc31bedb91e32066b671cb91
Lashi <lashi0812@gmail.com>
2024-09-18 13:11:34 +0000

    spilted into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Counter from "./Counter";
+import CounterButtons from "./CounterButtons";
+import ResetButton from "./ResetButton";
+import Title from "./Title";
+
 function App() {
-  return <p>Hello World!</p>;
+  return (
+    <main>
+      <div>
+        <Title />
+        <Counter />
+        <ResetButton />
+        <CounterButtons />
+      </div>
+    </main>
+  );
 }
 
 export default App;
